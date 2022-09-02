1a64cfdb3c69c6e832268a60c0c6b9c4826a12df
ateodocio <teodocio.am@gmail.com>
2022-09-02 06:26:16 -0500

    Implement basic mathematical operations

--- main.js
+++ main.js
@@ -1,2 +1,30 @@
-const time = document.querySelector(".time")
-time.textContent = new Date().toLocaleTimeString();
+
+window.onload = displayTime()
+
+function displayTime() {
+    const time = document.querySelector(".time")
+    time.textContent = new Date().toLocaleTimeString()
+    setTimeout(displayTime, 1000)
+}
+
+/* mathematical functions*/
+
+function add(x, y) {
+    return x + y
+}
+
+function subtract(x, y) {
+    return x - y
+}
+
+function multiply(x, y) {
+    return x * y
+}
+
+function divide(x, y) {
+    return x / y
+}
+
+function operate(operator, x, y) {
+    return operator(x, y)
+}
