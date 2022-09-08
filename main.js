5c1a4e952633e62c267cb07d2647301da0903810
ateodocio <teodocio.am@gmail.com>
2022-09-08 07:53:59 -0500

    Remove history display, implement math operations and clear button functionality, and fix display styling

--- main.js
+++ main.js
@@ -7,7 +7,9 @@ function displayTime() {
     setTimeout(displayTime, 1000)
 }
 
-/* mathematical functions*/
+
+
+// mathematical functions
 
 function add(x, y) {
     return x + y
@@ -26,25 +28,93 @@ function divide(x, y) {
 }
 
 function operate(operator, x, y) {
-    return operator(x, y)
+    switch (operator) {
+        case "add":
+            return add(x, y)
+        case "subtract":
+            return subtract(x, y)
+        case "multiply":
+            return multiply(x, y)
+        case "divide":
+            return divide(x, y)
+    }
 }
 
-/* button functionality */
 
-let digitCount = 0;
+
+// global variables
+let digitCount = 0
+let decimalCount = 0
+let stored = []
+let calculated = 0
+let currOperator = ""
+let operand = ""
+
+
+
+// button and display functionality
 
 const buttons = document.querySelectorAll("button")
 const currDisplay = document.querySelector(".currentDisplay")
-buttons.forEach(btn => btn.addEventListener("click", e => displayCharacter(e)))
+buttons.forEach(btn => btn.addEventListener("click", e => applyUserInput(e)))
 
 
-function displayCharacter(e) {
+
+function applyUserInput(e) {
+    console.log("count " + digitCount)
+    console.log(operand)
+    // checks what button was pressed by user then calls other handler functions
+    if (e.target.classList[0] === "num") {
+        displayNumber(e)
+        storeOperand(e)
+    } else if (e.target.classList[0] === "clear")
+        clearAll()
+
+    if (digitCount > 0) { // a number should exist before a decimal or an operator
+        if (e.target.classList[0] === "operator") {
+            storeUserInput(e)
+            if (stored.length === 2) {calculateExpression(e)}
+            decimalCount === 1 ? decimalCount -- : decimalCount = 0 // resets back to zero 
+        } else if (e.target.classList[0] === "decimal" && decimalCount === 0) { // only 1 decimal allowed per operand
+            decimalCount++
+            displayDecimal(e)
+            storeOperand(e)
+        }
+    }
+}
+
+// non-operational related functions
+function clearAll() {
+    currDisplay.textContent = ""
+    digitCount = 0
+    stored = []
+    calculated = 0
+    currOperator = ""
+    operand = ""
+}
+
+
+// history display
+function updateDisplay() {
+    currDisplay.textContent = calculated
+}
+
+
+
+// number-related functions 
+
+function storeOperand(e) {
+    operand += e.target.innerHTML
+}
+
+
+function displayNumber(e) {
     const lengthBoundaries = [7, 9, 13]
 
-    if (digitCount % 3 === 0 && digitCount !== 0 && digitCount < 15) {
-        currDisplay.textContent += ","
-    }
-    if ((digitCount < 15) && (e.target.classList[0] === "num")) {
+    // if (digitCount % 3 === 0 && digitCount !== 0 && digitCount < 15) {
+    //     currDisplay.textContent += ","
+    // }
+    if (digitCount < 15) {
         digitCount++
         currDisplay.textContent += e.target.innerHTML
         
@@ -54,10 +124,10 @@ function displayCharacter(e) {
     }
 }
 
+
 function getDisplayCharLength() {
     return (currDisplay.innerHTML).length
 }
-
 
 function changeDisplayFont(charLength) {
     switch (charLength) {
@@ -72,4 +142,33 @@ function changeDisplayFont(charLength) {
             break;
     }
 }
+
+function displayDecimal(e) {
+    currDisplay.textContent += e.target.innerHTML
+}
+
+
+
+// operator-related functions
+function storeUserInput(e) {
+    currDisplay.textContent += e.target.innerHTML
+    stored.push(Number(operand))  // change to Number type in order to be calculable
+    operand = ""
+    if (stored.length === 1) {
+        currOperator = e.target.classList[1]
+    }
+    console.log(stored)
+}
+
+
+
+function calculateExpression(e) {
+    calculated = operate(currOperator, stored[0], stored[1])
+    updateDisplay()
+    stored = [calculated]
+    digitCount++
+    
+}
+
+
 
